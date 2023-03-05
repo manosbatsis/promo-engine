5368cc427189975ceec21a70da72c8cfe84488b9
manosbatsis <manosbatsis@gmail.com>
2023-03-05 11:45:42 +0200

    fix(promotion): refactor quantity and bundle promotion types to fixed price

--- promo-engine-simple/src/main/java/com/github/manosbatsis/promo/engine/simple/promotion/ProductQuantityFixedPricePromotion.java
+++ promo-engine-simple/src/main/java/com/github/manosbatsis/promo/engine/simple/promotion/ProductQuantityFixedPricePromotion.java
@@ -3,21 +3,25 @@ package com.github.manosbatsis.promo.engine.simple.promotion;
 import com.github.manosbatsis.promo.engine.api.DiscountType;
 import com.github.manosbatsis.promo.engine.api.Promotion;
 import com.github.manosbatsis.promo.engine.api.PromotionPriority;
+import java.math.BigDecimal;
 import lombok.Getter;
 
 @Getter
-public class ProductQuantityPromotion implements Promotion {
+public class ProductQuantityFixedPricePromotion implements Promotion {
 
   private final String promotionId;
   private final String skuId;
   private final Integer quantity;
+  private final BigDecimal price;
   private final PromotionPriority promotionPriority =
       SimplePromotionPriority.CLASS_EXCLUSIVE_PRODUCT;
   private final DiscountType discountType = SimpleDiscountType.FIXED_PRICE;
 
-  public ProductQuantityPromotion(String promotionId, String skuId, Integer quantity) {
+  public ProductQuantityFixedPricePromotion(
+      String promotionId, String skuId, Integer quantity, BigDecimal price) {
     this.promotionId = promotionId;
     this.skuId = skuId;
     this.quantity = quantity;
+    this.price = price;
   }
 }
--- promo-engine-simple/src/main/java/com/github/manosbatsis/promo/engine/simple/promotion/ProductsBundleFixedPricePromotion.java
+++ promo-engine-simple/src/main/java/com/github/manosbatsis/promo/engine/simple/promotion/ProductsBundleFixedPricePromotion.java
@@ -3,20 +3,23 @@ package com.github.manosbatsis.promo.engine.simple.promotion;
 import com.github.manosbatsis.promo.engine.api.DiscountType;
 import com.github.manosbatsis.promo.engine.api.Promotion;
 import com.github.manosbatsis.promo.engine.api.PromotionPriority;
+import java.math.BigDecimal;
 import java.util.Set;
 import lombok.Getter;
 
 @Getter
-public class ProductsBundlePromotion implements Promotion {
+public class ProductsBundleFixedPricePromotion implements Promotion {
 
   private final String promotionId;
   private final Set<String> skus;
+  private final BigDecimal price;
   private final PromotionPriority promotionPriority =
       SimplePromotionPriority.CLASS_EXCLUSIVE_PRODUCT;
   private final DiscountType discountType = SimpleDiscountType.FIXED_PRICE;
 
-  public ProductsBundlePromotion(String promotionId, Set<String> skus) {
+  public ProductsBundleFixedPricePromotion(String promotionId, Set<String> skus, BigDecimal price) {
     this.promotionId = promotionId;
     this.skus = skus;
+    this.price = price;
   }
 }
